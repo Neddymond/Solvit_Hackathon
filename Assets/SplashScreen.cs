17032cd278eeba884755fe15b535f3afa9517913
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2019-09-01 16:47:39 +0100

    Final Touches to the Source Code, Environment, and UI.

---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreen : MonoBehaviour {
+
+    bool ShouldFade;
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("FadeOut", 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(ShouldFade)
+        {
+            gameObject.GetComponent<CanvasGroup>().alpha -= 0.01f;
+        }
+        if (gameObject.GetComponent<CanvasGroup>().alpha <= 0) gameObject.SetActive(false);
+	}
+
+    void FadeOut()
+    {
+        ShouldFade = true;
+    }
+
+    
+}
